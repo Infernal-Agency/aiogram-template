6657a4c2c633cc18d1a396a9fdeb28bcb24402de
KilleryStark <krutikovily@gmail.com>
2022-11-05 12:57:26 +0300

    first commit

---
+++ telegram_bot/data/config.py
@@ -0,0 +1,19 @@
+import os
+
+
+
+BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
+admins = [
+    os.getenv(ADMINS)
+]
+
+ip = os.getenv("ip")
+
+aiogram_redis = {
+    'host': ip,
+}
+
+redis = {
+    'address': (ip, 6379),
+    'encoding': 'utf8'
+}
